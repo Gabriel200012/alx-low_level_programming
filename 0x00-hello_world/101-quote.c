faa6261b2849852a777ae53059c803803a5ebfc0
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-03-16 04:55:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,16 +7,8 @@
  */
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2023-04-16\n";
-long 1 = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-_asm_("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (1));
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+while (*str)
+	write(STDERR_FILENO, str++, 1);
 return (1);
 }

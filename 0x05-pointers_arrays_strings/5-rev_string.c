2a60a0a000e44846a2248e79dda84fa05685852e
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-03-27 09:53:18 +0100

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
- * rev_string - prints reversed 
+ * rev_string - prints reversed string,followed
+ * by a new line
  * @s: pointer to the string to print
  * Return: void
 */
+
 void rev_string(char *s)
 {
 int len, i, half;

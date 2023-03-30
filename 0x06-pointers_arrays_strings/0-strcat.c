d90b0929d454b2537cb1489da6d960884e48a364
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-03-30 06:55:22 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++);
-do
-{
-a++;
-dest[i] = src[a];
-i++;
-}
-while (src[a] != '\0');
+int c, c2;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0; src[c2] ; c2++)
+dest[c++] = src[c2];
 return (dest);
 }

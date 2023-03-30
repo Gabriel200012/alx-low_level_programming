7acabe722595094e7eb4bedb5e1335688e7d7c64
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-03-30 06:59:33 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,16 +9,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  
-int dest_len, i;
- 
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
- 
+int c, i;
+c = 0;
+while (dest[c])
+c++;
 for (i = 0; i < n && src[i] != '\0'; i++)
-dest[dest_len + i] = src[i];
- 
-dest[dest_len + i] = '\0';
- 
+dest[c + i] = src[i];
+dest[c + i] = '\0';
 return (dest);
- 
 }

e8bb795ef3971bd7b7735db7b987bceb75138da1
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-05-08 12:08:37 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,8 @@ return (-1);
 if (!text_content)
 text_content = "";
 
-for (nletters = 0; text_content[nletters]; nletters++);
+for (nletters = 0; text_content[nletters]; nletters++)
+;
 
 rwr = write(fd, text_content, nletters);
 
